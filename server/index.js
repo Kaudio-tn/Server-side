a4f4f679f5df9b965cd7241ecbdd059ea35e7e4e
MohamedAmineYousfi <mohamedamineyousfi@gmail.com>
2021-11-26 00:48:23 +0100

    del

--- server/index.js
+++ server/index.js
@@ -13,13 +13,21 @@ app.get('/', (req,res)=> { //get method
   res.send('Hello World') //send response
 })
 
-
+//USERS CRUD 
 app.get('/users', (req,res)=> { //get method
   db.getAllUsers(req,res)
 })
 
-app.get('/users/:id', (req,res)=> { //get method
+app.get('/users/:id', (req,res)=> { //get one user method
   db.getOneUser(req,res)
+})
+
+app.delete('/users/:id', (req,res)=> { //del method
+  db.delOneUser(req,res)
+})
+
+app.post('/users',   (req,res)=>{
+  db.createUser(req,res)
 })
 
 app.listen(PORT,console.log(`Express is running on http://localhost:${PORT}`))
